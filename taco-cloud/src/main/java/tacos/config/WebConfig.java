782cbea94b7c49c98eb1b1f15288222cef48ce74
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-03 10:00:52 +0800

    第二章：配置实现控制器，终于生效了devTools

---
+++ taco-cloud/src/main/java/tacos/config/WebConfig.java
@@ -0,0 +1,17 @@
+package tacos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 定义控制器跳转的另一种方式
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
