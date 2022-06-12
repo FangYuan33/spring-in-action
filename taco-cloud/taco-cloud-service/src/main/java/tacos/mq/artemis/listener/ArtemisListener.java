1f457f9c32aeef9c919feb9aaa005dbc0547e005
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-12 18:18:32 +0800

    第八章：Artemis的监听器实现

---
+++ taco-cloud/taco-cloud-service/src/main/java/tacos/mq/artemis/listener/ArtemisListener.java
@@ -0,0 +1,20 @@
+package tacos.mq.artemis.listener;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import tacos.domain.Order;
+
+@Slf4j
+@Component
+public class ArtemisListener {
+
+    /**
+     * Artemis的监听器实现
+     */
+    @JmsListener(destination = "${artemis.orderQueue}")
+    public void receiveOrder(Order order) {
+        log.info("Artemis监听器收到订单消息: {}", JSON.toJSONString(order));
+    }
+}
