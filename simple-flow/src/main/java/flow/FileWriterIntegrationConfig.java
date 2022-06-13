248e900f6bbc03a3ec9f42380265c6a180e09c9b
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-13 22:17:17 +0800

    第九章：添加一个DSL配置

--- simple-flow/src/main/java/flow/FileWriterIntegrationConfig.java
+++ simple-flow/src/main/java/flow/FileWriterIntegrationConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.integration.transformer.GenericTransformer;
 
@@ -42,5 +46,19 @@ public class FileWriterIntegrationConfig {
         handler.setAppendNewLine(true);
 
         return handler;
+    }
+
+    /**
+     * JAVA DSL 配置
+     */
+    @Bean
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlows
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(String::toUpperCase)
+                .channel(MessageChannels.direct("fileWriterChannel"))
+                .handle(Files.outboundAdapter(new File("/Users/wangyilong/GitHub"))
+                        .fileExistsMode(FileExistsMode.APPEND).appendNewLine(true))
+                .get();
     }
 }
