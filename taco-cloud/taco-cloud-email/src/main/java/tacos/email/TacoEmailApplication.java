60b3d62c9b85d28a64d44f48ea93086df0b672f6
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-16 15:29:35 +0800

    第九章：更新了下依赖

--- taco-cloud/taco-cloud-email/src/main/java/tacos/email/TacoEmailApplication.java
+++ taco-cloud/taco-cloud-email/src/main/java/tacos/email/TacoEmailApplication.java
@@ -2,10 +2,11 @@ package tacos.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TacoEmailApplication {
 
 	public static void main(String[] args) {
--- taco-cloud/taco-cloud-start/src/main/java/tacos/TacoCloudApplication.java
+++ taco-cloud/taco-cloud-start/src/main/java/tacos/TacoCloudApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "tacos")
 @MapperScan("tacos.repository.mapper")
+@SpringBootApplication(scanBasePackages = "tacos")
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
