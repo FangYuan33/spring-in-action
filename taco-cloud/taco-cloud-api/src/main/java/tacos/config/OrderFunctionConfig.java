3a937963059eb115d45aab5692e1a33b94f6a202
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-19 16:57:10 +0800

    第十一章：写好了order函数式请求处理器

--- taco-cloud/taco-cloud-api/src/main/java/tacos/config/OrderFunctionConfig.java
+++ taco-cloud/taco-cloud-api/src/main/java/tacos/config/OrderFunctionConfig.java
@@ -1,14 +1,15 @@
 package tacos.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tacos.domain.Order;
+import tacos.dto.TacoOrderDto;
+import tacos.mq.artemis.consumer.OrderMessageConsumer;
 import tacos.service.OrderService;
 
 
@@ -17,11 +18,97 @@ public class OrderFunctionConfig {
 
     @Autowired
     private OrderService orderService;
+    @Autowired(required = false)
+    private OrderMessageConsumer orderMessageConsumer;
+
+    private static final String PREFIX = "orders";
 
     @Bean
     public RouterFunction<ServerResponse> orderRouterFunction() {
-        return RouterFunctions.route(RequestPredicates.GET("orders"),
-                request -> ServerResponse.ok().body(Flux.fromIterable(orderService.list()), Order.class)
-        );
+        return RouterFunctions.route(RequestPredicates.GET(PREFIX), this::list)
+                .andRoute(RequestPredicates.GET(PREFIX + "/cookNextOrder"), this::cookNextOrder)
+                .andRoute(RequestPredicates.GET(PREFIX + "/{id}"), this::getOne)
+                .andRoute(RequestPredicates.POST(PREFIX), this::createOrder)
+                .andRoute(RequestPredicates.PUT(PREFIX + "/updateAll/{orderId}"), this::updateAllField)
+                .andRoute(RequestPredicates.PATCH(PREFIX + "/updateAny/{orderId}"), this::updateAnyField)
+                .andRoute(RequestPredicates.DELETE(PREFIX + "/{orderId}"), this::deleteOrder);
+    }
+
+    /**
+     * 列表
+     */
+    @NotNull
+    public Mono<ServerResponse> list(ServerRequest request) {
+        return ServerResponse.ok().body(Flux.fromIterable(orderService.list()), Order.class);
+    }
+
+
+    @NotNull
+    public Mono<ServerResponse> getOne(ServerRequest request) {
+        String id = request.pathVariable("id");
+        Order order = orderService.getById(id);
+
+        if (order != null) {
+            return ServerResponse.ok().body(Flux.just(order), Order.class);
+        } else {
+            return ServerResponse.ok().body(Flux.empty(), Void.TYPE);
+        }
+    }
+
+    @NotNull
+    public Mono<ServerResponse> createOrder(ServerRequest request) {
+        Mono<TacoOrderDto> orderDtoMono = request.bodyToMono(TacoOrderDto.class);
+
+        Mono<Mono<Object>> orderMono = orderDtoMono.map(item -> {
+            orderService.save(item);
+            return Mono.empty();
+        });
+
+        return ServerResponse.ok().body(orderMono, Void.TYPE);
+    }
+
+    /**
+     * PutMapping 语义上是这对整体的更新（大规模）
+     */
+    @NotNull
+    public Mono<ServerResponse> updateAllField(ServerRequest request) {
+        return ServerResponse.ok().body(updateById(request), Void.TYPE);
+    }
+
+    /**
+     * PatchMapping 语义上是对任一字段的更新
+     */
+    @NotNull
+    public Mono<ServerResponse> updateAnyField(ServerRequest request) {
+        return ServerResponse.ok().body(updateById(request), Void.TYPE);
+    }
+
+    private Mono<Mono<Object>> updateById(ServerRequest request) {
+        Mono<TacoOrderDto> orderDtoMono = request.bodyToMono(TacoOrderDto.class);
+        String orderId = request.pathVariable("orderId");
+
+        return orderDtoMono.map(item -> {
+            orderService.updateById(Long.parseLong(orderId), item);
+            return Mono.empty();
+        });
+    }
+
+    @NotNull
+    public Mono<ServerResponse> deleteOrder(ServerRequest request) {
+        String orderId = request.pathVariable("orderId");
+        orderService.deleteOrder(Long.parseLong(orderId));
+
+        return ServerResponse.ok().build();
+    }
+
+    @NotNull
+    public Mono<ServerResponse> cookNextOrder(ServerRequest request) {
+        Order order = orderMessageConsumer.receiveOrder();
+
+        if (order != null) {
+            return ServerResponse.ok().body(order, Order.class);
+        } else {
+            return ServerResponse.ok().build();
+        }
     }
 }
