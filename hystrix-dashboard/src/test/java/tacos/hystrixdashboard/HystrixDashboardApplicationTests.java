154084cf1ee0b8dcd4639e5b2108413aed21ea2a
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-26 17:40:31 +0800

    第十五章: hystrix-dashboard

---
+++ hystrix-dashboard/src/test/java/tacos/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.hystrixdashboard;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
