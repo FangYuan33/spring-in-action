9cd538b8c07b816f95f0c2e243bfac64edc9f823
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-06-29 21:22:46 +0800

    第十六章: 自定义 health Actuator

---
+++ taco-cloud/taco-cloud-api/src/main/java/tacos/config/MyHealthIndicator.java
@@ -0,0 +1,24 @@
+package tacos.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+
+/**
+ * 我自己的健康状况指示器
+ */
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (hour > 12) {
+            return Health.outOfService().withDetail("Why", "Hello, I Love My Life!").build();
+        }
+
+        return Health.up().withDetail("Why", "I Love My Work!").build();
+    }
+}
